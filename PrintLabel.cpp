14a05802299b00207811f0e0c9a4c093238628d8
Gregory Becker <gregorybecker@notonthehighstreet.com>
2015-12-17 14:02:12 +0000

    Add option to select size of labels

--- PrintLabel.cpp
+++ PrintLabel.cpp
@@ -41,27 +41,37 @@ int main(int argc, char** argv)
 {
   try
   {
-    if (argc < 2)
-      throw Error("Usage: PrintLabel <ImageName> [<ImageName> ...]");
-    
+    if (argc < 3)
+      throw Error("Usage: PrintLabel <labelSize> <ImageName> [<ImageName> ...]");
+
     cout << "Printers:" << endl;
     print_printers();
-    
+
     int             num_options = 0;
     cups_option_t*  options = NULL;
-    
-    //num_options = cupsAddOption("PageSize", "w162h90", num_options, &options); //11354 Multi-Purpose
-    num_options = cupsAddOption("PageSize", "w102h252", num_options, &options); //99012 Large address
+
+    std::string label_size argv[1];
+
+    if (label_size.compare("57x32mm") == 0) {
+      num_options = cupsAddOption("PageSize", "w162h90", num_options, &options); //11354 Multi-Purpose
+    } else if (label_size.compare("41x89mm") == 0) {
+      num_options = cupsAddOption("PageSize", "w118h252", num_options, &options); //11356 White Name Badge
+    } else if (label_size.compare("54x101mm") == 0) {
+      num_options = cupsAddOption("PageSize", "w154h286.2", num_options, &options); //99014 Shipping
+    } else {
+      throw Error("Label size: %s is not a valid size. Must be WxHmm. Check source code to find supported sizes", label_size);
+    }
+
     //num_options = cupsAddOption("scaling", "100", num_options, &options);
     //num_options = cupsAddOption("orientation-requested", "4", num_options, &options);
     //num_options = cupsAddOption("DymoHalftoning", "ErrorDiffusion", num_options, &options);
     //num_options = cupsAddOption("DymoPrintQuality", "Graphics", num_options, &options);
-    
-    for (int i=1; i<argc; i++) {
+
+    for (int i=2; i<argc; i++) {
       cupsPrintFile(PrinterName, argv[i], "Label", num_options, options);
     }
     cupsFreeOptions(num_options, options);
-    
+
     return 0;
   }
   catch(std::exception& e)
